f97b0b2f197c4341b71cab9391d3875070a27869
Miguel Saavedra <miguelernesto911@gmail.com>
2020-04-16 17:20:01 -0500

    Testingbranch (#2)
    * More features
    
    * Fix

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
-    access_key = "AKIAXKQJSOC3A25MXP6R"
-    secret_key = "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"
-    region     = "us-east-1"
+  profile    = "default"
+  region     = "us-east-1"
 }
 
-resource "aws_instance" "instance_test" {
-    ami             = "ami-039a49e70ea773ffc"
-    instance_type   = "t2.micro"
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }

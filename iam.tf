3452370869e92655bee24ed21c32c30e74dea0d0
Miguel Saavedra <miguelernesto911@gmail.com>
2020-04-16 17:58:58 -0500

    Added iam

--- iam.tf
+++ iam.tf
@@ -40,7 +40,13 @@ resource "aws_iam_role_policy" "test_policy" {
       "Action": "ec2:*",
       "Effect": "Allow",
       "Resource": "*"
-    }
+    },
+    {
+      "Sid": "IAMFullAcess",
+      "Action": "iam:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    },
   ]
 }
 EOF
